f6e0e4f8a901e3b018f81d81559fcea7f307ef3e
Chris B <chrismjboyle@gmail.com>
2019-09-10 12:20:56 +0100

    Added chroot helper script

---
+++ chrooter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]
+then
+  echo "Specify what disk the root is on..."
+  exit 1
+fi
+
+if [[ stat $1 ]]
+then
+  echo "$1 is not a file"
+  exit 1
+fi
+
+mount $1 /mnt/gentoo
+cd /mnt/gentoo
+mount --types proc /proc proc
+mount --rbind /sys sys
+mount --make-rslave sys
+mount --rbind /dev dev
+mount --make-rslave dev
+chroot . /bin/bash
